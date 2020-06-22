fd5c25a664d00776a3afe5befec97909e0782d3d
Deep Multani <Deep.Multani@sita.aero>
2020-06-22 12:04:13 -0400

    committing pub/sub c

---
+++ Receive/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class Receive
+{
+    public static void Main()
+    {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.UserName = "deep";
+        factory.Password = "sita2017";
+        factory.VirtualHost = "/";
+        factory.HostName = "52.224.128.255";
+        factory.Port = AmqpTcpEndpoint.UseDefaultPort;
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+
+            var queueName = channel.QueueDeclare().QueueName;
+            //channel.QueueDeclare(queue: "hello",
+            //                     durable: false,
+            //                     exclusive: false,
+            //                     autoDelete: false,
+            //                     arguments: null);
+
+            channel.QueueBind(queue: queueName,
+                                exchange: "PaxCheck",
+                                routingKey: "");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+            //channel.Close();
+            //connection.Close();
+        }
+    }
+}
---
+++ Send/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class Send
+{
+    public static void Main()
+    {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.UserName = "deep";
+        factory.Password = "sita2017";
+        factory.VirtualHost = "/";
+        //factory.AmqpUriSslProtocols = Protocols.FromEnvironment();
+        factory.HostName = "52.224.128.255";
+        factory.Port = AmqpTcpEndpoint.UseDefaultPort;
+       // IConnection conn = factory.CreateConnection();
+        //var factory = new ConnectionFactory() 
+        //{ 
+        //    HostName = "amqp://deep:sita2017@52.224.128.255:5672/"
+        //};
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+
+            //channel.QueueDeclare(queue: "hello",
+            //                     durable: false,
+            //                     exclusive: false,
+            //                     autoDelete: false,
+            //                     arguments: null);
+
+            channel.ExchangeDeclare("PaxCheck", "fanout", false, true, null);
+
+            string message = "";
+
+            while (message != "end")
+            {
+                message = Console.ReadLine();
+
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+
+                channel.BasicPublish(exchange: "PaxCheck",
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0} type end to exit", message);
+
+
+            }
+
+            
+            channel.Close();
+            //connection.Close();
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+        
+    }
+}
